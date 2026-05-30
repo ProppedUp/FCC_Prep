3fdafff0e8c2994083fa6ba8e0a90b7178b629f5
ProppedUp <proppedupmotors@protonmail.com>
2026-05-30 11:09:18 -0400

    Use network-first service worker caching

--- sw.js
+++ sw.js
@@ -1,5 +1,29 @@
-const CACHE='fccprep-v2';
+const CACHE='fccprep-v3';
 const ASSETS=['./','./index.html','./study.html','./manifest.webmanifest','./js/app_v10.js','./js/questionLoader.js','./css/style.css','./data/manifest.json','./data/element1.json','./data/element3.json','./data/element8.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const clone=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,clone));
+        return response;
+      })
+      .catch(()=>caches.match(event.request))
+  );
+});
