097a608cf35b60d9aaed03b79aea40db585cec3b
ProppedUp <proppedupmotors@protonmail.com>
2026-05-30 10:09:45 -0400

    Cache Element 8 and refresh service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fccprep-v1';
-const ASSETS=['./','./index.html','./study.html','./manifest.webmanifest','./js/app_v10.js','./js/questionLoader.js','./css/style.css','./data/manifest.json','./data/element1.json','./data/element3.json'];
+const CACHE='fccprep-v2';
+const ASSETS=['./','./index.html','./study.html','./manifest.webmanifest','./js/app_v10.js','./js/questionLoader.js','./css/style.css','./data/manifest.json','./data/element1.json','./data/element3.json','./data/element8.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
